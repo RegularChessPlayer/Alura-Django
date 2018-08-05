8274d7e07501d8c976c36afa14389debab208a9c
Thiago Rodrigues de Souza <thiago.engh@gmail.com>
2018-08-04 23:58:23 -0300

    Adicionado relacionamento entre tabelas

--- connectedin/perfis/urls.py
+++ connectedin/perfis/urls.py
@@ -1,7 +1,10 @@
-from perfis.views import index, exibir
+from perfis.views import index, exibir, convidar
 from django.urls import path, re_path
+
 
 urlpatterns = [
     re_path(r'^$', index, name='index'),
     re_path(r'^perfis/(?P<perfil_id>\d+)$', exibir, name='exibir'),
+    re_path(r'^perfis/(?P<perfil_id>\d+)/convidar$', convidar, name='convidar'),
+
 ]
